3ae553c8e6ecc87ab392875f1e7034e04c9dc74b
Ali Nauman Qureshi <alinauman97@outlook.com>
2020-04-14 21:17:00 +0500

    Enabled routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { CartComponent } from './cart/cart.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'cart', component: CartComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
