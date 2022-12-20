39ef0351d72197bd0edbe2b10984e994c49a1d80
Ali Nauman <alinauman97@outlook.com>
2022-12-21 00:29:38 +0500

    Fix broken navigation

--- src/app/shared/navigation-bar/navigation-bar.component.ts
+++ src/app/shared/navigation-bar/navigation-bar.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navigation-bar',
   templateUrl: './navigation-bar.component.html',
   styleUrls: ['./navigation-bar.component.css'],
   standalone: true,
+  imports: [BrowserModule, RouterModule],
 })
-export class NavigationBarComponent {}
+export class NavigationBarComponent {
+  links = [
+    {
+      name: 'Products',
+      link: '/products',
+    },
+    {
+      name: 'Cart',
+      link: '/cart',
+    },
+  ];
+}
