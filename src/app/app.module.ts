26f872ffca0ec58621d885fdf9108675e4019c81
Ali Nauman <alinauman97@outlook.com>
2022-12-21 22:09:06 +0500

    Remove old card-page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CartPageComponent } from './components/cart-page/cart-page.component';
 import { NavigationBarComponent } from './shared';
 
 @NgModule({
-  declarations: [AppComponent, CartPageComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, NavigationBarComponent],
   providers: [],
   bootstrap: [AppComponent],
