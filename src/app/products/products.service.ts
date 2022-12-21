f484bb2c319b32b1f575bc68ad104890640d9c04
Ali Nauman <alinauman97@outlook.com>
2022-12-21 23:05:29 +0500

    Remove getByCategory

--- src/app/products/products.service.ts
+++ src/app/products/products.service.ts
@@ -1,8 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { filter } from 'rxjs/operators';
-
-import { ProductCategory } from './product-category.enum';
 
 import { Product } from './product.model';
 
@@ -14,15 +11,5 @@ export class ProductsService {
 
   get() {
     return this.httpClient.get<Product[]>('/assets/products.json');
-  }
-
-  getByCategory(category: ProductCategory) {
-    return (
-      this.httpClient
-        .get<Product[]>('/assets/products.json')
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        .pipe(filter(products => products.filter(p => p.category === category)))
-    );
   }
 }
