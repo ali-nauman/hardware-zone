62be19abf1a3a34cfb989b476162e001a98f4336
Ali Nauman <alinauman97@outlook.com>
2022-12-21 22:33:21 +0500

    Use Observables and async pipe

--- src/app/cart/cart/cart.component.ts
+++ src/app/cart/cart/cart.component.ts
@@ -1,24 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CartItem } from '../cart-item.model';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css'],
 })
-export class CartComponent {
-  cartItems = [];
-  totalCost: number;
+export class CartComponent implements OnInit {
+  cartItems$!: Observable<CartItem[]>;
+  totalCost = 0;
 
   constructor(private cartService: CartService) {}
 
   ngOnInit(): void {
-    this.cartItems = this.cartService.getItems();
+    this.cartItems$ = this.cartService.getItems();
     this.totalCost = this.cartService.getTotalCost();
   }
 
   clearCart(): void {
     this.cartService.clearCart();
-    this.cartItems = [];
     this.totalCost = 0;
   }
 }
