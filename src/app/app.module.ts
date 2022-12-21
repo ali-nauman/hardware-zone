282aba263e0926813d0e3f7ff42b1ec099deb618
Ali Nauman <alinauman97@outlook.com>
2022-12-21 22:02:54 +0500

    Remove old product-card, product-page components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CartPageComponent } from './components/cart-page/cart-page.component';
-import { ProductCardComponent } from './components/product-card/product-card.component';
-import { ProductsPageComponent } from './components/products-page/products-page.component';
 import { NavigationBarComponent } from './shared';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductsPageComponent,
-    CartPageComponent,
-    ProductCardComponent,
-  ],
+  declarations: [AppComponent, CartPageComponent],
   imports: [BrowserModule, AppRoutingModule, NavigationBarComponent],
   providers: [],
   bootstrap: [AppComponent],
