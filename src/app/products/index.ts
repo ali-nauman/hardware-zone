fb7dc93c211309a65432dba8966c224bc1373caf
Ali Nauman <alinauman97@outlook.com>
2022-12-21 22:08:18 +0500

    Add products barrel file

---
+++ src/app/products/index.ts
@@ -0,0 +1,5 @@
+export * from './product-category.enum';
+export * from './product.model';
+export * from './products.module';
+export * from './products.service';
+export * from './products/products.component';
