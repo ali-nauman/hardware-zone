cf34ae66c96ff2d750fa700cfd0135a85e1c54d8
Ali Nauman <alinauman97@outlook.com>
2023-01-25 21:00:44 +0500

    Bootstrap app through standalone AppComponent

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,32 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { HttpClientModule } from '@angular/common/http';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { AppComponent } from './app/app.component';
 
-import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(HttpClientModule),
+    provideRouter([
+      {
+        path: 'products',
+        loadComponent: () => import('./app/products/products/products.component').then(m => m.ProductsComponent),
+      },
+      {
+        path: 'cart',
+        loadComponent: () => import('./app/cart/cart/cart.component').then(m => m.CartComponent),
+      },
+      {
+        path: '',
+        redirectTo: 'products',
+        pathMatch: 'full',
+      },
+    ]),
+  ],
+});
